bb6b9e0699afaff11efc27363bee96c92789ae57
Atharv Pote <atharvpote@gmail.com>
2022-10-18 15:00:31 +0530

    Added search and location button.

--- components/currentWeather.tsx
+++ components/currentWeather.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-import { MdLocationOn } from "react-icons/md";
+import { MdLocationOn, MdGpsFixed } from "react-icons/md";
 import clear from "../public/Clear.png";
 
 export default function CurrentWeather(): JSX.Element {
   return (
-    <section className="medium-dark-background grid min-h-screen content-center">
+    <section className="medium-dark-background grid min-h-screen">
       <div>
+        <div className="flex items-center justify-between px-4 pt-6">
+          <button className="light-grey-background py-2 px-4 text-white shadow-md shadow-gray-900">
+            Search for places
+          </button>
+          <span className="light-grey-background inline-block rounded-full p-2 shadow-md shadow-gray-900">
+            <MdGpsFixed className="text-2xl" />
+          </span>
+        </div>
         <div className="relative grid h-80 place-content-center after:absolute after:top-0 after:h-full after:w-full after:bg-[url('../public/Cloud-background.png')] after:bg-cover after:bg-center after:opacity-10">
           <div className="mx-auto w-36">
             <Image
