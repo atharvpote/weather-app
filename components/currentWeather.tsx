1d7479c5477d9849d4f2eb69df0ef1cd6f997e5d
Atharv Pote <atharvpote@gmail.com>
2022-10-19 19:43:32 +0530

    Test.

--- components/currentWeather.tsx
+++ components/currentWeather.tsx
@@ -35,13 +35,13 @@ export default function CurrentWeather({
           </button>
         </div>
         <div className="relative mx-auto grid h-80 max-w-[588px] place-content-center after:absolute after:top-0 after:h-full after:w-full after:bg-[url('../public/Cloud-background.png')] after:bg-cover after:bg-center after:opacity-10">
-          <div className="mx-auto w-36 bg-white">
+          <div className="mx-auto w-36">
             <Image
               src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
               alt=""
               width={100}
               height={100}
-              layout="responsive"
+              // layout="responsive"
               className="opacity-100"
             />
           </div>
