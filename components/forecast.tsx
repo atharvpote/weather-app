6c16149bae9b3ef58bec673ce031e74ce3ac3bd2
Atharv Pote <atharvpote@gmail.com>
2022-10-21 01:59:35 +0530

    Try

--- components/forecast.tsx
+++ components/forecast.tsx
@@ -14,7 +14,7 @@ export default function Forecast({ forecastData }: Props): JSX.Element {
   console.log(forecast);
   return (
     <div className=" mx-6 flex flex-wrap justify-center gap-6 pt-8 pb-16">
-      {cards(forecast)}
+      {/* {cards(forecast)} */}
     </div>
   );
 }
