abca18799ffb4eee8520a314deb357ef9537aa15
Atharv Pote <atharvpote@gmail.com>
2022-10-20 19:36:39 +0530

    Refactor.

--- components/forecast.tsx
+++ components/forecast.tsx
@@ -1,9 +1,9 @@
 import Image, { StaticImageData } from "next/image";
 import { format } from "date-fns";
-import type {
-  DailyForecastData,
-  WeatherForecastData,
-} from "../utils/getWeatherData";
+import type { WeatherForecastData } from "../utils/getWeatherData";
+import extractForecastData, {
+  ForecastData,
+} from "../utils/extractForecastData";
 import transparent from "../public/Transparent.png";
 
 type Props = {
@@ -16,11 +16,10 @@ export default function Forecast({
   forecastData,
   getIcon,
 }: Props): JSX.Element {
-  console.log(forecastData);
+  let forecast: ForecastData = {};
+  if (forecastData?.list) forecast = extractForecastData(forecastData?.list);
 
-  const list = forecastData?.list;
-
-  if (list) extractForecastData(list);
+  console.log(forecast);
 
   return (
     <div className=" mx-6 flex flex-wrap justify-center gap-6 pt-8 pb-16">
@@ -47,72 +46,4 @@ export default function Forecast({
       })}
     </div>
   );
-}
-
-function extractForecastData(list: DailyForecastData[]): void {
-  type WeatherData = {
-    date: string;
-    minTemp: number;
-    maxTemp: number;
-    weatherId: number;
-  };
-
-  console.log(list);
-
-  const dates = new Map<string, WeatherData[]>();
-
-  list?.forEach((data) => {
-    if (!dates.has(data.dt_txt.split(" ")[0].split("-")[2]))
-      dates.set(data.dt_txt.split(" ")[0].split("-")[2], []);
-
-    dates.get(data.dt_txt.split(" ")[0].split("-")[2])?.push({
-      date: data.dt_txt,
-      minTemp: data.main.temp_min,
-      maxTemp: data.main.temp_max,
-      weatherId: data.weather[0].id,
-    });
-  });
-
-  const aggregatedData: {
-    [key: string]: {
-      minTemp: number | null;
-      maxTemp: number | null;
-      sorted: [number, number][];
-    };
-  } = {};
-
-  for (const arr of Array.from(dates.entries())) {
-    const date = arr[0];
-    const weatherData = arr[1];
-    const weatherIdCounter = new Map<number, number>();
-
-    let minTemp: number | null = null;
-    let maxTemp: number | null = null;
-
-    for (const values of weatherData) {
-      if (!minTemp) minTemp = values.minTemp;
-      else if (minTemp > values.minTemp) minTemp = values.minTemp;
-
-      if (!maxTemp) maxTemp = values.maxTemp;
-      else if (maxTemp < values.maxTemp) maxTemp = values.maxTemp;
-
-      const weatherCount = weatherIdCounter.get(values.weatherId);
-      if (!weatherCount) weatherIdCounter.set(values.weatherId, 1);
-      else weatherIdCounter.set(values.weatherId, weatherCount + 1);
-    }
-
-    const sorted = Array.from(weatherIdCounter.entries()).sort((a, b) => {
-      if (a[1] - b[1] > 0) return -1;
-      if (a[1] - b[1] < 0) return 1;
-      else return 0;
-    });
-
-    aggregatedData[date] = {
-      minTemp,
-      maxTemp,
-      sorted,
-    };
-  }
-
-  console.log(aggregatedData);
 }
---
+++ utils/extractForecastData.ts
@@ -0,0 +1,77 @@
+import type { DailyForecastData } from "./getWeatherData";
+
+export type ForecastData = {
+  [key: string]: {
+    minTemp: number | null;
+    maxTemp: number | null;
+    weatherId: number;
+  };
+};
+
+export default function extractForecastData(
+  list: DailyForecastData[]
+): ForecastData {
+  type WeatherData = {
+    date: string;
+    minTemp: number;
+    maxTemp: number;
+    weatherId: number;
+  };
+
+  const separatedData = new Map<string, WeatherData[]>();
+
+  list?.forEach((data) => {
+    if (!separatedData.has(data.dt_txt.split(" ")[0]))
+      separatedData.set(data.dt_txt.split(" ")[0], []);
+
+    separatedData.get(data.dt_txt.split(" ")[0])?.push({
+      date: data.dt_txt,
+      minTemp: data.main.temp_min,
+      maxTemp: data.main.temp_max,
+      weatherId: data.weather[0].id,
+    });
+  });
+
+  const forecastData: {
+    [key: string]: {
+      minTemp: number | null;
+      maxTemp: number | null;
+      weatherId: number;
+    };
+  } = {};
+
+  for (const arr of Array.from(separatedData.entries())) {
+    const date = arr[0];
+    const weatherData = arr[1];
+    const IdCounter = new Map<number, number>();
+
+    let minTemp: number | null = null;
+    let maxTemp: number | null = null;
+
+    for (const dataPoint of weatherData) {
+      if (!minTemp) minTemp = dataPoint.minTemp;
+      else if (minTemp > dataPoint.minTemp) minTemp = dataPoint.minTemp;
+
+      if (!maxTemp) maxTemp = dataPoint.maxTemp;
+      else if (maxTemp < dataPoint.maxTemp) maxTemp = dataPoint.maxTemp;
+
+      const weatherCount = IdCounter.get(dataPoint.weatherId);
+      if (!weatherCount) IdCounter.set(dataPoint.weatherId, 1);
+      else IdCounter.set(dataPoint.weatherId, weatherCount + 1);
+    }
+
+    const commonWeatherID = Array.from(IdCounter.entries()).sort((a, b) => {
+      if (a[1] - b[1] > 0) return -1;
+      if (a[1] - b[1] < 0) return 1;
+      else return 0;
+    })[0][0];
+
+    forecastData[date] = {
+      minTemp,
+      maxTemp,
+      weatherId: commonWeatherID,
+    };
+  }
+
+  return forecastData;
+}
